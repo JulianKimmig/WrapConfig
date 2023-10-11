cc4e04cef2ce9f102e5f700ecc93e98a5435a141
Julian Kimmig <julian.kimmig@gmx.net>
2023-10-11 11:56:56 +0200

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import config_manager
+
+# Read the requirements from requirements.txt
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="config_manager",
+    version=config_manager.__version__,
+    description="Wraper to manage configurations",
+    author="Julian Kimmig",
+    author_email="julian.kimmig@linkdlab.de",
+    packages=["config_manager"],  # Update with your package name
+    install_requires=requirements,
+)
