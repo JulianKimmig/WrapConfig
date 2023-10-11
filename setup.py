4eba3da92ae8a9496b4aad0d1de17308fe5a460b
Julian Kimmig <julian.kimmig@gmx.net>
2023-10-11 12:52:48 +0200

    license

--- setup.py
+++ setup.py
@@ -13,4 +13,12 @@ setup(
     author_email="julian.kimmig@linkdlab.de",
     packages=["config_manager"],  # Update with your package name
     install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",  # Adjust to your Python version
+    ],
 )
