0410233f43fac25cd649978b34649aa96a2b2475
JulianKimmig <julian.kimmig@gmx.net>
2025-02-05 12:29:19 +0100

    Refactor tests to use mocked write_file method for JSON, TOML, and YAML configurations

--- tests/test_jsonconfig.py
+++ tests/test_jsonconfig.py
@@ -22,28 +22,32 @@ class TestJSONWrapConfig(unittest.TestCase):
     def test_save_file(self):
         from wrapconfig import JSONWrapConfig
 
-        # Mock the open method and os methods
-        m = mock_open()
-        with patch("builtins.open", m), patch(
-            "os.path.exists", return_value=False
-        ), patch("os.makedirs"):
-            manager = JSONWrapConfig(self.dummy_path)
+        with (
+            patch.object(JSONWrapConfig, "_write_file") as mocked_write_file,
+            patch("os.makedirs") as mocked_makedirs,
+        ):
+            manager = JSONWrapConfig(self.dummy_path, default_save=False)
             manager.set_data(self.data)
             manager.save()
-            written_data = "".join(m().write.call_args_list[-1][0])
 
-            self.assertEqual(json.dumps(self.data, indent=4), written_data)
+            # We expect JSON dump to be created with default_flow_style=False.
+            expected_dump = json.dumps(self.data, indent=4)
+
+            mocked_write_file.assert_called_once_with(expected_dump)
+      
 
     def test_save_file_existing_dir(self):
         from wrapconfig import JSONWrapConfig
 
         # Mock the open method and os methods
         m = mock_open(read_data=json.dumps(self.data))
-        with patch("builtins.open", m), patch(
-            "os.path.exists", return_value=True  # Mock to return True
-        ), patch(
-            "os.makedirs",
-        ) as mock_makedirs:
+        with (
+            patch("builtins.open", m),
+            patch("os.path.exists", return_value=True),  # Mock to return True
+            patch(
+                "os.makedirs",
+            ) as mock_makedirs,
+        ):
             manager = JSONWrapConfig(self.dummy_path)
             manager.set_data(self.data)
             manager.save()
